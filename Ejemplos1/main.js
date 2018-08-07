1c4f1a9e31dd73b7c84816daebb943accd647c69
geloma <germanlm93@gmail.com>
2018-08-07 15:58:51 -0500

    SUA template

---
+++ Ejemplos1/main.js
@@ -0,0 +1,14 @@
+// Mostrar texto
+console.log("1. HOLA");
+
+// Sumar numeros
+var a = 1;
+var b = 2;
+var c = a + b;
+console.log("2. SUMA: " + c);
+
+//Mostrar varios numeros
+console.log("\nRango de numeros");
+for(var i=0; i<=3; i++){
+	console.log("Esta es la i => ", i); 
+}
