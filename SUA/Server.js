e16d8ce312ce20ab66af18673e90d0a319584f55
geloma <germanlm93@gmail.com>
2018-08-15 21:32:05 -0500

    Clase 2

--- SUA/Server.js
+++ SUA/Server.js
@@ -2,8 +2,20 @@ var express = require('express');
 var app     = express();
 var path    = require("path");
 
+app.get('/company/create', function (req, res) {
+  res.sendFile(
+    path.join(
+      __dirname, '/src/views/company/create.html'
+    )
+  );
+});
+
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname+'/src/views/index.html'));
+  res.sendFile(
+    path.join(
+      __dirname, '/src/views/index.html'
+    )
+  );
 });
 
 app.listen(3000, function () {
